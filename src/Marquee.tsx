3dade7b7686cdffdfa9ce02b093f447f465631d6
agorapilled <johngardner13@outlook.com>
2025-01-21 17:11:06 -0500

    v0

--- src/Marquee.tsx
+++ src/Marquee.tsx
@@ -3,9 +3,9 @@ import Marquee from 'react-fast-marquee';
 
 const MarqueeComponent: React.FC = () => {
   return (
-    <div style={{ backgroundColor: 'red', color: 'white', padding: '10px' }}>
+    <div style={{ backgroundColor: '#D1B28E', color: 'white', padding: '10px' }}>
       <Marquee className="font-semibold text-2xl" speed={70} pauseOnHover gradient={false} loop={0}>
-        BREAKING: Microsoft Unveils Immortalized Kanye AI Called "YEI"! EXCLUSIVE: YEI Sparks Controversy Over Copyright and Creativity! DRAMA: Is the World Ready for an Eternal Kanye? INSIDER REPORTS: YEI to Drop Its First Album—"808s and Eternity"! SHOCKING: Critics Question the Ethics of Kanye's Immortality!&nbsp;
+        BREAKING: Microsoft Unveils Immortalized Trump AI Named "Poteus Maximus"! EXCLUSIVE: Poteus Maximus Raises Questions About AI and Human Identity! DRAMA: Is the World Ready for an Eternal Trump? INSIDER REPORTS: Poteus Maximus to Drop Its First Album—"MAGA Millions"! SHOCKING: Critics Question the Ethics of Trump's Immortality!&nbsp;
       </Marquee>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -38,9 +38,9 @@ const completionEndpoint = import.meta.env?.VITE_COMPLETION_ENDPOINT || 'http://
 
 import './styles.css'
 
-const AGENT_ID = 'f9473937-3a5e-0c90-ba50-7c075effad23' // this comes from the agentId output from running the Eliza framework, it likely will be in uuid format, i.e. '123e4567-e89b-12d3-a456-426614174000'
-const SIMLI_FACE_ID = '1373a78a-29a9-4ebf-8249-c1639f6301ba'
-const ELEVENLABS_VOICE_ID = 'QVdfyuce5vtARk1420LS'
+const AGENT_ID = 'e0e10e6f-ff2b-0d4c-8011-1fc1eee7cb32' // this comes from the agentId output from running the Eliza framework, it likely will be in uuid format, i.e. '123e4567-e89b-12d3-a456-426614174000'
+const SIMLI_FACE_ID = 'b8ef6b37-bcb0-4a74-b712-13530a477dd2'
+const ELEVENLABS_VOICE_ID = 'UNp0QMV47KW9Kr2tPU1G'
 
 const simliClient = new SimliClient()
 
@@ -221,17 +221,12 @@ const App = () => {
     <>
       <GlobalStyles />
       <ThemeProvider theme={original}>
-      <div className='flex h-screen w-full flex-col items-center justify-center font-mono text-white bg-[#C0C0C0]'>
-        <video
-          autoPlay
-          loop
-          muted
-          playsInline
-          className='absolute inset-0 w-full h-full object-cover'
-        >
-          <source src='/bg.mp4' type='video/mp4' />
-        </video>
-        <div className={`relative w-[30%] ${startWebRTC && !isConnecting ? '' : 'hidden'}`}>
+      <div
+        className="flex h-screen w-full flex-col items-center justify-center font-mono text-white"
+        style={{ backgroundImage: 'url(/b.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}
+      >
+        <div className='absolute h-full w-full bg-black opacity-60'></div>
+        <div className={`relative w-[75%] md:w-[30%] ${startWebRTC && !isConnecting ? '' : 'hidden'}`}>
           <Window>
             <video
               ref={videoRef}
@@ -248,8 +243,8 @@ const App = () => {
             <div className='w-screen absolute top-0'>
               <MarqueeComponent />
             </div>
-            <a href="https://x.com/YEIwtf" className='absolute bottom-4 right-4 text-black'>
-              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50">
+            <a href="https://x.com/maximuspoteus" className='absolute bottom-4 right-4 text-black'>
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50" fill='#FFFFFF'>
                 <path d="M 11 4 C 7.1456661 4 4 7.1456661 4 11 L 4 39 C 4 42.854334 7.1456661 46 11 46 L 39 46 C 42.854334 46 46 42.854334 46 39 L 46 11 C 46 7.1456661 42.854334 4 39 4 L 11 4 z M 11 6 L 39 6 C 41.773666 6 44 8.2263339 44 11 L 44 39 C 44 41.773666 41.773666 44 39 44 L 11 44 C 8.2263339 44 6 41.773666 6 39 L 6 11 C 6 8.2263339 8.2263339 6 11 6 z M 13.085938 13 L 22.308594 26.103516 L 13 37 L 15.5 37 L 23.4375 27.707031 L 29.976562 37 L 37.914062 37 L 27.789062 22.613281 L 36 13 L 33.5 13 L 26.660156 21.009766 L 21.023438 13 L 13.085938 13 z M 16.914062 15 L 19.978516 15 L 34.085938 35 L 31.021484 35 L 16.914062 15 z"></path>
               </svg>
             </a>
@@ -258,13 +253,16 @@ const App = () => {
               onSubmit={handleSubmit}
               className="fixed bottom-4 left-1/2 transform -translate-x-1/2 flex justify-center w-full max-w-md space-x-2 px-4"
             >
-              <div className="flex items-center space-x-2">
+              <div className="flex items-center space-x-2 relative">
+                <div className="absolute -top-5 left-1/2 -translate-x-1/2 text-[10px] md:text-xs whitespace-nowrap">
+                  *response may take a few seconds with high demand
+                </div>
                 <Window>
                   <input
                     type="text"
                     value={inputText}
                     onChange={(e) => setInputText(e.target.value)}
-                    placeholder="talk to ye"
+                    placeholder="talk to poteus"
                     className="grow bg-black px-3 py-2 text-white outline-none focus:outline-none focus:ring-0"
                   />
                 </Window>
@@ -284,32 +282,37 @@ const App = () => {
           <>
             {isConnecting && (
               <p className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-white'>
-                Connecting...
+                Uploading Poteus...
               </p>
             )}
             {!isConnecting && (
-              <div className='h-screen w-screen flex flex-col justify-center items-center relative bg-black'>
-                <div className='z-10 group rounded-full border border-black/5 bg-neutral-100 text-base md:text-lg lg:text-xl text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800 mb-8 md:mb-16'>
-                  <div className="inline-flex items-center justify-center px-4 py-1 transition ease-out text-neutral-600">
+              <div
+                className="h-screen w-screen flex flex-col justify-center items-center relative"
+                style={{ backgroundImage: 'url(/b.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}
+              >
+                <div className='absolute h-full w-full bg-black opacity-60'></div>
+                <div className='z-10 group rounded-full border border-black/5 bg-neutral-100 text-base md:text-lg lg:text-xl text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800 mb-6 md:mb-12'>
+                  <div className="inline-flex items-center justify-center px-4 py-1 transition ease-out text-[#D1B28E]">
                     <span>version 0.1</span>
                   </div>
                 </div>
 
-                <div className="z-10 text-white font-custom text-xl md:text-3xl lg:text-5xl xl:text-6xl mb-4">
-                  Kan(ye) Intelligence
+                <div className="z-10 text-[#D1B28E] font-custom text-4xl md:text-7xl lg:text-8xl xl:text-9xl mb-1">
+                  Poteus Maximus
                 </div>
 
                 <button
                   disabled={isConnecting}
                   onClick={handleStart}
-                  className="z-10 bg-white text-black font-semibold px-6 py-2 rounded-full mt-4 md:mt-10 md:text-xl lg:text-3xl md:hover:bg-neutral-600 md:hover:text-white transition duration-100"
+                  className="z-10 bg-white text-[#D1B28E] font-semibold px-6 py-2 rounded-full mt-2 md:mt-6 md:text-2xl lg:text-4xl md:hover:bg-[#D1B28E] md:hover:text-white transition duration-100"
                 >
                   access
                 </button>
-
-                <div className="absolute inset-0 pointer-events-none">
-                  <div className="bg-repeat w-full h-full opacity-50" style={{ backgroundImage: "url('/k2.gif')" }}></div>
-                </div>
+                <a href="https://x.com/maximuspoteus" className='absolute bottom-4 right-4 text-black'>
+                  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50" fill='#FFFFFF'>
+                    <path d="M 11 4 C 7.1456661 4 4 7.1456661 4 11 L 4 39 C 4 42.854334 7.1456661 46 11 46 L 39 46 C 42.854334 46 46 42.854334 46 39 L 46 11 C 46 7.1456661 42.854334 4 39 4 L 11 4 z M 11 6 L 39 6 C 41.773666 6 44 8.2263339 44 11 L 44 39 C 44 41.773666 41.773666 44 39 44 L 11 44 C 8.2263339 44 6 41.773666 6 39 L 6 11 C 6 8.2263339 8.2263339 6 11 6 z M 13.085938 13 L 22.308594 26.103516 L 13 37 L 15.5 37 L 23.4375 27.707031 L 29.976562 37 L 37.914062 37 L 27.789062 22.613281 L 36 13 L 33.5 13 L 26.660156 21.009766 L 21.023438 13 L 13.085938 13 z M 16.914062 15 L 19.978516 15 L 34.085938 35 L 31.021484 35 L 16.914062 15 z"></path>
+                  </svg>
+                </a>
               </div>
             )}
           </>
