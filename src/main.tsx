be2a85beb6b1b7bc926d31f020bb142012cc1c34
agorapilled <johngardner13@outlook.com>
2025-01-22 18:23:59 -0500

    v0.1

--- src/main.tsx
+++ src/main.tsx
@@ -59,7 +59,7 @@ const App = () => {
   const [roomID, setRoomID] = useState('')
   const [userUUID, setUserUUID] = useState('')
   const [copied, setCopied] = useState(false);
-  const ca = 'XXXXXXXXXX';
+  const ca = 'XXXXXXXXX';
 
   const handleCopy = () => {
     navigator.clipboard.writeText(ca).then(() => {
